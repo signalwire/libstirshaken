75299a8e8c64e68f223876a4e5e1c8b284fd267c
Piotr Gregor <piotr@dataandsignal.com>
2020-08-28 17:27:42 +0000

    Fixes

--- test/stir_shaken_test_13.c
+++ test/stir_shaken_test_13.c
@@ -10,7 +10,9 @@ const char *path = "./test/run";
         printf("Error code is: '%d'\n", error_code); \
     }
 
-stir_shaken_status_t stir_shaken_unit_test_hash(void)
+stir_shaken_sp_t sp;
+
+stir_shaken_status_t stir_shaken_unit_test_sp_cert_req(void)
 {
     EVP_PKEY *pkey = NULL;
     stir_shaken_status_t status = STIR_SHAKEN_STATUS_FALSE;
