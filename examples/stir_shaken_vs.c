5c5a41c3647d2f650aa18f972376671be9f49cdd
Piotr Gregor <piotr@signalwire.com>
2020-10-23 13:45:56 +0000

    Update VS example (cloud domain)

--- examples/stir_shaken_vs.c
+++ examples/stir_shaken_vs.c
@@ -4,29 +4,30 @@
 /**
  * This example demonstrates how to create simplest verification service (STI-SP/VS).
  * We are using here reference PASSporT (you can find it in test/ref/sp folder) we created with this command:
- *		./stirshaken passport-create --privkey test/ref/sp/sp.priv --url http://shaken.signalwire.com/sp.pem --vvv -f passport.txt
+ *		./stirshaken passport-create --privkey test/ref/sp/sp.priv --url http://shaken.signalwire.cloud/sp.pem --vvv -f passport.txt
+ *
  * PASSporT is:
- * 
+ *
  * {
- *    "alg": "ES256",
- *    "ppt": "shaken",
- *    "typ": "passport",
- *    "x5u": "http://shaken.signalwire.com/sp.pem"
+ *     "alg": "ES256",
+ *     "ppt": "shaken",
+ *     "typ": "passport",
+ *     "x5u": "http://shaken.signalwire.cloud/sp.pem"
  * }
  * .
  * {
- *    "attest": "A",
- *    "dest": "{\"tn\":\"01256500600\"}",
- *    "iat": 1599258938,
- *    "orig": "{\"tn\":\"01256789999\"}",
- *    "origid": "ref"
+ *     "attest": "A",
+ *     "dest": "{\"tn\":\"01256500600\"}",
+ *     "iat": 1603458131,
+ *     "orig": "{\"tn\":\"01256789999\"}",
+ *     "origid": "ref"
  * }
- * 
- * PASSporT encoded (signed) is:
- * eyJhbGciOiJFUzI1NiIsInBwdCI6InNoYWtlbiIsInR5cCI6InBhc3Nwb3J0IiwieDV1IjoiaHR0cDovL3NoYWtlbi5zaWduYWx3aXJlLmNvbS9zcC5wZW0ifQ.eyJhdHRlc3QiOiJBIiwiZGVzdCI6IntcInRuXCI6XCIwMTI1NjUwMDYwMFwifSIsImlhdCI6MTU5OTI1ODkzOCwib3JpZyI6IntcInRuXCI6XCIwMTI1Njc4OTk5OVwifSIsIm9yaWdpZCI6InJlZiJ9.p_lhqTk-zBBNcsZgv5gNmO63xrbvapMwZmqmN2NwfbiJB2VxBait5EeUxgDpFs30EC7r4cm8tQD8CV2gFkFEtw
- * 
- * SIP Identity Header is:
- * eyJhbGciOiJFUzI1NiIsInBwdCI6InNoYWtlbiIsInR5cCI6InBhc3Nwb3J0IiwieDV1IjoiaHR0cDovL3NoYWtlbi5zaWduYWx3aXJlLmNvbS9zcC5wZW0ifQ.eyJhdHRlc3QiOiJBIiwiZGVzdCI6IntcInRuXCI6XCIwMTI1NjUwMDYwMFwifSIsImlhdCI6MTU5OTI1ODkzOCwib3JpZyI6IntcInRuXCI6XCIwMTI1Njc4OTk5OVwifSIsIm9yaWdpZCI6InJlZiJ9.URaXwA1TcXJtqfbGKT_FH14y8KgCGM4mrJW8ApdEb2bhstrErjDMSEY1llsAV_zxcWpyIf5hUIk_XI4WpkVACw;info=<http://shaken.signalwire.com/sp.pem>;alg=ES256;ppt=shaken
+ *
+ * PASSporT signed is:
+ * eyJhbGciOiJFUzI1NiIsInBwdCI6InNoYWtlbiIsInR5cCI6InBhc3Nwb3J0IiwieDV1IjoiaHR0cDovL3NoYWtlbi5zaWduYWx3aXJlLmNsb3VkL3NwLnBlbSJ9.eyJhdHRlc3QiOiJBIiwiZGVzdCI6IntcInRuXCI6XCIwMTI1NjUwMDYwMFwifSIsImlhdCI6MTYwMzQ1ODEzMSwib3JpZyI6IntcInRuXCI6XCIwMTI1Njc4OTk5OVwifSIsIm9yaWdpZCI6InJlZiJ9.G_6hnwPGAeUalviElXbxl4kKR5qenib6fRrCP-cgwKN2hMsSTXYjIFEhl_VqmeTB8dk9fidroDlFe8dPdyPy3g
+ *
+ * SIP Identity Header with PASSporT is:
+ * eyJhbGciOiJFUzI1NiIsInBwdCI6InNoYWtlbiIsInR5cCI6InBhc3Nwb3J0IiwieDV1IjoiaHR0cDovL3NoYWtlbi5zaWduYWx3aXJlLmNsb3VkL3NwLnBlbSJ9.eyJhdHRlc3QiOiJBIiwiZGVzdCI6IntcInRuXCI6XCIwMTI1NjUwMDYwMFwifSIsImlhdCI6MTYwMzQ1ODEzMSwib3JpZyI6IntcInRuXCI6XCIwMTI1Njc4OTk5OVwifSIsIm9yaWdpZCI6InJlZiJ9.cNI-uIirMOiT19OcQag2UYjHWTgTqtr5jhSk3KxflqSC7FbrrYDr51zCEvzDMoETpge7eQeQ6ASVzb1dhVVhKQ;info=<http://shaken.signalwire.cloud/sp.pem>;alg=ES256;ppt=shaken
  **/
 
 stir_shaken_status_t cache_callback(stir_shaken_callback_arg_t *arg)
@@ -44,17 +45,17 @@ stir_shaken_status_t cache_callback(stir_shaken_callback_arg_t *arg)
 			// Here, we supply libstirshaken with certificate we cached earlier, avoiding HTTP(S) download.
 			// We must return STIR_SHAKEN_STATUS_HANDLED to signal this to the library, otherwise it would execute HTTP(S) download
 
-			if (!strcmp("http://shaken.signalwire.com/sp.pem", arg->cert.public_url)) {
+			if (!strcmp("http://shaken.signalwire.cloud/sp.pem", arg->cert.public_url)) {
 
-				printf("Supplying certificate from the cache: %s...", arg->cert.public_url);
+				printf("Supplying certificate from the cache: %s...\n", arg->cert.public_url);
 
 				if (!(cert_cached.x = stir_shaken_load_x509_from_file(&ss, "examples/cache/sp.pem"))) {
-					printf("Cannot load X509 from file");
+					printf("Cannot load X509 from file\n");
 					goto exit;
 				}
 
 				if (STIR_SHAKEN_STATUS_OK != stir_shaken_cert_copy(&ss, &arg->cert, &cert_cached)) {
-					printf("Cannot copy certificate");
+					printf("Cannot copy certificate\n");
 					goto exit;
 				}
 
@@ -85,10 +86,8 @@ void run_verification_service(stir_shaken_callback_t callback)
 	stir_shaken_error_t		error_code = STIR_SHAKEN_ERROR_GENERAL;
 	stir_shaken_status_t	status = STIR_SHAKEN_STATUS_FALSE;
 
-	char *passport_encoded = "eyJhbGciOiJFUzI1NiIsInBwdCI6InNoYWtlbiIsInR5cCI6InBhc3Nwb3J0IiwieDV1IjoiaHR0cDovL3NoYWtlbi5zaWduYWx3aXJlLmNvbS9zcC5wZW0ifQ.eyJhdHRlc3QiOiJBIiwiZGVzdCI6IntcInRuXCI6XCIwMTI1NjUwMDYwMFwifSIsImlhdCI6MTU5OTI1ODkzOCwib3JpZyI6IntcInRuXCI6XCIwMTI1Njc4OTk5OVwifSIsIm9yaWdpZCI6InJlZiJ9.p_lhqTk-zBBNcsZgv5gNmO63xrbvapMwZmqmN2NwfbiJB2VxBait5EeUxgDpFs30EC7r4cm8tQD8CV2gFkFEtw";
-
-	char *sip_identity_header = "eyJhbGciOiJFUzI1NiIsInBwdCI6InNoYWtlbiIsInR5cCI6InBhc3Nwb3J0IiwieDV1IjoiaHR0cDovL3NoYWtlbi5zaWduYWx3aXJlLmNvbS9zcC5wZW0ifQ.eyJhdHRlc3QiOiJBIiwiZGVzdCI6IntcInRuXCI6XCIwMTI1NjUwMDYwMFwifSIsImlhdCI6MTU5OTI1ODkzOCwib3JpZyI6IntcInRuXCI6XCIwMTI1Njc4OTk5OVwifSIsIm9yaWdpZCI6InJlZiJ9.URaXwA1TcXJtqfbGKT_FH14y8KgCGM4mrJW8ApdEb2bhstrErjDMSEY1llsAV_zxcWpyIf5hUIk_XI4WpkVACw;info=<http://shaken.signalwire.com/sp.pem>;alg=ES256;ppt=shaken";
-
+	char *passport_encoded = "eyJhbGciOiJFUzI1NiIsInBwdCI6InNoYWtlbiIsInR5cCI6InBhc3Nwb3J0IiwieDV1IjoiaHR0cDovL3NoYWtlbi5zaWduYWx3aXJlLmNsb3VkL3NwLnBlbSJ9.eyJhdHRlc3QiOiJBIiwiZGVzdCI6IntcInRuXCI6XCIwMTI1NjUwMDYwMFwifSIsImlhdCI6MTYwMzQ1ODEzMSwib3JpZyI6IntcInRuXCI6XCIwMTI1Njc4OTk5OVwifSIsIm9yaWdpZCI6InJlZiJ9.cNI-uIirMOiT19OcQag2UYjHWTgTqtr5jhSk3KxflqSC7FbrrYDr51zCEvzDMoETpge7eQeQ6ASVzb1dhVVhKQ;info=<http://shaken.signalwire.cloud/sp.pem>;alg=ES256;ppt=shaken";
+	char *sip_identity_header = "eyJhbGciOiJFUzI1NiIsInBwdCI6InNoYWtlbiIsInR5cCI6InBhc3Nwb3J0IiwieDV1IjoiaHR0cDovL3NoYWtlbi5zaWduYWx3aXJlLmNsb3VkL3NwLnBlbSJ9.eyJhdHRlc3QiOiJBIiwiZGVzdCI6IntcInRuXCI6XCIwMTI1NjUwMDYwMFwifSIsImlhdCI6MTYwMzQ1ODEzMSwib3JpZyI6IntcInRuXCI6XCIwMTI1Njc4OTk5OVwifSIsIm9yaWdpZCI6InJlZiJ9.cNI-uIirMOiT19OcQag2UYjHWTgTqtr5jhSk3KxflqSC7FbrrYDr51zCEvzDMoETpge7eQeQ6ASVzb1dhVVhKQ;info=<http://shaken.signalwire.cloud/sp.pem>;alg=ES256;ppt=shaken";
 	stir_shaken_passport_t	passport = {0};
 	stir_shaken_cert_t		*cert = NULL;
 	int		iat_freshness_seconds = INT_MAX;
